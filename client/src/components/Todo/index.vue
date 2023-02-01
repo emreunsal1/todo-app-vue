cd9f08a6b42ce37f2194675a97509d3c83adbd23
Emre Ünsal <unsalemre111@gmail.com>
2023-02-01 21:05:11 +0300

    forgotten import is added

--- client/src/components/Todo/index.vue
+++ client/src/components/Todo/index.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="todo">
-      <div class="todo__left">
+    <div class="todo__left">
       <CheckBox :todo-id="todo._id" />
       <TodoContent :todo-id="todo._id" />
     </div>
     <div class="todo__right">
-      <img class="todo__right__delete" src="../../assets/Delete.svg" @click="deleteTodo(todo._id)" />
-      <Select :options="selectOptions" @change="updatePriority" :default-value="todo.priority" />
+      <img
+        class="todo__right__delete"
+        src="../../assets/Delete.svg"
+        @click="deleteTodo(todo._id)"
+      />
+      <Select
+        :options="selectOptions"
+        @change="updatePriority"
+        :default-value="todo.priority"
+      />
     </div>
     <div class="todo__border"></div>
   </div>
@@ -15,27 +23,27 @@
 import TodoContent from "../TodoContent/index.vue";
 import Priority from "../Priority/index.vue";
 import Select from "../Select/index.vue";
-import {mapActions} from "vuex";
+import { mapActions } from "vuex";
+import CheckBox from "../CheckBox/index.vue";
 
 export default {
   name: "ListItem.vue",
-  components: {Select, Priority, TodoContent},
-  data(){
+  components: { Select, Priority, TodoContent, CheckBox },
+  data() {
     return {
       isSelectOpened: false,
-      selectOptions: [{ text: "Low", value: "low", icon: "low" },
+      selectOptions: [
+        { text: "Low", value: "low", icon: "low" },
         { text: "Mid", value: "mid", icon: "mid" },
-        { text: "High", value: "high" , icon: "high"}]
-    }
+        { text: "High", value: "high", icon: "high" },
+      ],
+    };
   },
-  methods:{
-    ...mapActions([
-      'deleteTodo',
-      'updateTodoById'
-    ]),
-    updatePriority(value){
-      this.updateTodoById({_id: this.todo._id, priority: value})
-    }
+  methods: {
+    ...mapActions(["deleteTodo", "updateTodoById"]),
+    updatePriority(value) {
+      this.updateTodoById({ _id: this.todo._id, priority: value });
+    },
   },
   props: ["todo"],
 };
@@ -48,39 +56,39 @@ export default {
   margin: 0 -75px;
   padding: 10px 75px;
   position: relative;
-  &:not(:last-child){
+  &:not(:last-child) {
     margin-bottom: 50px;
   }
 
-  &__border{
+  &__border {
     content: "";
     width: calc(100% - 150px);
     height: 2px;
-    background-color: #C6C6C6;
+    background-color: #c6c6c6;
     left: 50%;
     bottom: -15px;
     transform: translateX(-50%);
     position: absolute;
   }
-  &__left{
+  &__left {
     cursor: pointer;
     display: flex;
     align-items: center;
     width: 100%;
   }
-  &__right{
+  &__right {
     display: flex;
-    &__delete{
+    &__delete {
       opacity: 0;
       visibility: hidden;
     }
   }
-  &:hover{
-    .select{
+  &:hover {
+    .select {
       display: flex;
     }
-    background-color: #F4F6FF;
-    .todo__right__delete{
+    background-color: #f4f6ff;
+    .todo__right__delete {
       cursor: pointer;
       opacity: 1;
       visibility: visible;
@@ -88,7 +96,7 @@ export default {
     }
     .priority {
       display: none;
-      .svg-container{
+      .svg-container {
         display: none;
       }
     }
